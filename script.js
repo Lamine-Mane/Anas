ac5b48bfa038c08d123bf196a34ef1b66e60f9c4
Lamine MANE <langmane95@gmail.com>
2023-10-27 11:57:40 +0200

    modification du fichier js: ajout du temps d'affichage

--- script.js
+++ script.js
@@ -17,20 +17,23 @@ fetch(
     options.forEach((option) => Selection.appendChild(option));
 
     Selection.addEventListener("click", function () {
-      fetch(
-        "https://data.enseignementsup-recherche.gouv.fr/api/explore/v2.1/catalog/datasets/fr-esr-atlas_regional-effectifs-d-etudiants-inscrits/records?limit=20&refine=annee_universitaire%3A%222022-23%22&refine=sexe_de_l_etudiant%3A%22Feminin%22&refine=geo_nom%3A%22Toulouse%22"
-      )
-        .then((response) => response.json())
-        .then((results) => {
-          console.log(results);
-          listItems.innerHTML = "";
-          const ligne = results.results.map((liste) => {
-            const li = document.createElement("li");
-            li.textContent = liste.effectif;
-            return li;
+      listItems.innerHTML = "";
+      setTimeout(() => {
+        fetch(
+          "https://data.enseignementsup-recherche.gouv.fr/api/explore/v2.1/catalog/datasets/fr-esr-atlas_regional-effectifs-d-etudiants-inscrits/records?limit=20&refine=annee_universitaire%3A%222022-23%22&refine=sexe_de_l_etudiant%3A%22Feminin%22&refine=geo_nom%3A%22Toulouse%22"
+        )
+          .then((response) => response.json())
+          .then((results) => {
+            console.log(results);
+            listItems.innerHTML = "";
+            const ligne = results.results.map((liste) => {
+              const li = document.createElement("li");
+              li.textContent = liste.effectif;
+              return li;
+            });
+            console.log(ligne);
+            ligne.forEach((li) => listItems.appendChild(li));
           });
-          console.log(ligne);
-          ligne.forEach((li) => listItems.appendChild(li));
-        });
+      }, 2000);
     });
   });
